c2f0f90d7cf0dedc1a6104db6e6d6a0976112cdf
Zuvvo <Zuvvo4@gmail.com>
2022-10-27 18:30:12 +0200

    bot testing

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -1,4 +1,8 @@
 class BotController < ApplicationController
+  require 'discordrb'
+
+  attr_reader :bot
+
   def index
     @buttons_obj = ''
   end
@@ -6,14 +10,27 @@ class BotController < ApplicationController
   def create
     test_run_bot if params[:run]
     nameless_test if params[:test]
+    close_bot if params[:close]
   end
 
   def test_run_bot
-    puts "running bot?"
+    @bot = Discordrb::Bot.new token: Rails.application.credentials.dig(:discord, :token)#, client_id: Rails.application.credentials.dig(:discord, :app_id)
+
+    bot.message(with_text: 'Ping!') do |event|
+      event.respond 'Pong!'
+    end
+
+    bot.run
   end
 
   def nameless_test
     puts "test?"
+  end
+
+  def close_bot
+    @bot = Discordrb::Bot.new token: Rails.application.credentials.dig(:discord, :token)
+    puts "bot nil? #{bot.nil?}"
+    bot.stop unless bot.nil?
   end
 
 

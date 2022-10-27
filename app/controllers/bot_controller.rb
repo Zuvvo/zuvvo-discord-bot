9a11fd1b175b7914fd1f34168e13b5b0aebea330
Zuvvo <Zuvvo4@gmail.com>
2022-10-27 16:42:52 +0200

    bot buttons heroku test

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -1,12 +1,20 @@
 class BotController < ApplicationController
   def index
+    @buttons_obj = ''
   end
 
   def create
-    test_run_bot
+    test_run_bot if params[:run]
+    nameless_test if params[:test]
   end
 
   def test_run_bot
-
+    puts "running bot?"
   end
+
+  def nameless_test
+    puts "test?"
+  end
+
+
 end

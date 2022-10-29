e654b89c802fc9e916d6c0894f628cebf403e8d1
Zuvvo <Zuvvo4@gmail.com>
2022-10-29 13:14:41 +0200

    application.rb additional paths

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module ZuvvoDiscordBot
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load = true
+    config.eager_load_paths += %W(#{config.root}/app/bot)
   end
 end

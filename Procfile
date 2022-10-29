8d96931970e0e9e355a654655762dbee701f6113
Zuvvo <Zuvvo4@gmail.com>
2022-10-29 12:05:23 +0200

    procfile edit

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-web: bin/rails server -p ${PORT:-5000} -e $RAILS_ENV
-release: bin/rails db:migrate
 worker: bundle exec ruby app/bot/bot_init.rb
--- app/bot/bot_init.rb
+++ app/bot/bot_init.rb
@@ -3,7 +3,7 @@ require 'discordrb'
 bot = Discordrb::Bot.new token: ENV['DISCORD_TOKEN']
 
 bot.message(with_text: 'Ping!') do |event|
-  event.respond 'Pong localhost bot_init.rb'
+  event.respond 'Pong production!'
 end
 
 bot.run

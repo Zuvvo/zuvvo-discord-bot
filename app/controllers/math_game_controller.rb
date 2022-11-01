81b12b6123058e80686499deb08cd7d18b5bc054
Zuvvo <Zuvvo4@gmail.com>
2022-11-02 00:16:25 +0100

    math_game submit form

--- app/controllers/math_game_controller.rb
+++ app/controllers/math_game_controller.rb
@@ -1,14 +1,17 @@
 class MathGameController < ApplicationController
   def new
-
+    @math_game = MathGame.new
   end
 
   def create
-    puts "create request"
+    p = math_game_params
+    user = DiscordUser.find_or_create_by(name: p[:username])
+    game = MathGame.new(discord_user: user, riddle: p[:riddle], time: p[:time], difficulty: p[:difficulty], points: p[:points])
+    game.save
   end
 
   def math_game_params
-    #params.require(:message).permit(:body, :spot_id)
+    params.require(:math_game).permit(:username, :riddle, :time, :difficulty, :points)
   end
 
 end

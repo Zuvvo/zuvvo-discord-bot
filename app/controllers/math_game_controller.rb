5308e20ae9fcbf4a1a49290a8470dbba6caa24c3
Zuvvo <Zuvvo4@gmail.com>
2022-11-11 20:27:02 +0100

    displaying db data on main page

--- app/controllers/math_game_controller.rb
+++ app/controllers/math_game_controller.rb
@@ -9,8 +9,8 @@ class MathGameController < ApplicationController
   def create
     p = params
     user = DiscordUser.find_or_create_by(name: p[:host])
-    MathGame.new(discord_user: user, time: p[:time],
-                        difficulty: p[:difficulty], riddles_count: p[:riddles_count],
+    MathGame.new(discord_user: user, time: p[:time].to_i,
+                        difficulty: p[:difficulty].to_i, riddles_count: p[:riddles_count].to_i,
                         results: p[:results]).save
   end
 

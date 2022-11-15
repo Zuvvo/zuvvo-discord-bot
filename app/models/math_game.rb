1880989b3811c309ffafef75e8f3307126ea926a
Zuvvo <Zuvvo4@gmail.com>
2022-11-15 11:22:30 +0100

    resolved test problems

--- app/models/math_game.rb
+++ app/models/math_game.rb
@@ -1,22 +1,24 @@
 class MathGame < ApplicationRecord
   belongs_to :discord_user
 
-  def winner
+  def winners
     scoreboard = hash_results
-    scoreboard.key(scoreboard.values.max)
+    max = scoreboard.values.max
+    scoreboard.select { |k, v| v == max }.keys
   end
 
   def players
-    hash_results.keys.join(', ')
+    hash_results.keys
   end
 
   private
+
   def hash_results
     output = {}
-    scores = results[1..-1].scan(/\[(.*?)\]/)
+    scores = results[1..].scan(/\[(.*?)\]/)
     scores.each do |arr|
       s = arr[0].split(', ')
-      output[s[0].gsub("\"", '')] = s[1].to_i
+      output[s[0].gsub('"', '')] = s[1].to_i
     end
     output
   end
--- spec/models/math_game_spec.rb
+++ spec/models/math_game_spec.rb
@@ -16,25 +16,25 @@ describe MathGame, type: :model do
   it 'checks if players are listed correctly in MathGame' do
     user = build(:discord_user)
     game = create(:math_game, discord_user: user, results: "[[\"One\", 2], [\"Two\", 2], [\"Three\", 1]]" )
-    expect(game.players).to eq("One, Two, Three")
+    expect(game.players).to eq(['One', 'Two', 'Three'])
   end
 
   it 'checks if player is a winner with one player with the most points scored' do
     user = build(:discord_user)
     game = create(:math_game, discord_user: user, results: "[[\"One\", 5], [\"Two\", 2], [\"Three\", 1]]" )
-    expect(game.winner).to eq("One")
+    expect(game.winners).to eq(['One'])
   end
 
   it 'checks if player is a winner with two players with the most points scored' do
     user = build(:discord_user)
     game = create(:math_game, discord_user: user, results: "[[\"One\", 5], [\"Two\", 5], [\"Three\", 1]]" )
-    expect(game.players).to eq(["One", "Two"])
+    expect(game.winners).to eq(['One', 'Two'])
   end
 
   it 'checks if player is a winner with two players with the most points scored, one on the bottom of scoreboard' do
     user = build(:discord_user)
     game = create(:math_game, discord_user: user, results: "[[\"One\", 5], [\"Two\", 2], [\"Three\", 2], [\"Four\", 5]]" )
-    expect(game.players).to eq(["One", "Four"])
+    expect(game.winners).to eq(['One', 'Four'])
   end
 
 end
